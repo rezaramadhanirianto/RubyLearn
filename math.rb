ad3f1d5a28e09a9ee75ad952d026a30175f57546
RezaRamadhanIrianto <rezaramadhanirianto@gmail.com>
2020-02-05 12:06:05 +0700

    asdf

---
+++ math.rb
@@ -0,0 +1,17 @@
+# Variable and Interpolation
+# Order of aritmatik proses # Makin Atas Makin utama
+# Pemdas
+# Presentase ()
+# Exponensial **
+# Multipication *
+# Division /
+# Addition +
+# Substraction -
+# even -> genap odd->ganjil
+puts 1 - 2 + 3 *4 
+puts 1 + 2 - 3 /4.0
+puts 1 * (2 - 3) / 4.0
+puts 1 ** 2 - (3 / 4.0)
+
+puts 15.even?
+puts 16.odd?
