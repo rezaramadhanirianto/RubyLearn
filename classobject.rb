1b36fcef496e50019a4f3686a9f16b934f5284c3
RezaRamadhanIrianto <rezaramadhanirianto@gmail.com>
2020-02-19 15:23:25 +0700

    Master

--- classobject.rb
+++ classobject.rb
@@ -1,122 +1,417 @@
-# class Calculator
-#     def penjumlahan(a, b)
-#         a + b 
+# # # # # # # # # class Calculator
+# # # # # # # # #     def penjumlahan(a, b)
+# # # # # # # # #         a + b 
+# # # # # # # # #     end
+# # # # # # # # #     def pengurangan(a)
+# # # # # # # # #         a - 10
+# # # # # # # # #     end
+# # # # # # # # # end
+# # # # # # # # # calc = Calculator.new
+# # # # # # # # # hasil_penjumlahan = calc.penjumlahan(18, 5)
+# # # # # # # # # puts hasil_penjumlahan
+# # # # # # # # # hasil_pengurangan = calc.pengurangan(18)
+# # # # # # # # # puts hasil_pengurangan
+
+# # # # # # # # # class CalculatorKool
+# # # # # # # # #     def initialize(a, b)
+# # # # # # # # #         @parameter_a = a
+# # # # # # # # #         @parameter_b = b
+# # # # # # # # #         @lokal = a + b
+# # # # # # # # #     end
+# # # # # # # # #     def penjumlahan
+# # # # # # # # #         @parameter_a + @parameter_b
+# # # # # # # # #     end
+# # # # # # # # #     def perkalian
+# # # # # # # # #         @parameter_a * @parameter_b
+# # # # # # # # #     end
+# # # # # # # # # end
+
+# # # # # # # # # calc = CalculatorKool.new(10, 5) #Giving parameter Default variable
+# # # # # # # # # hasil_plus = calc.penjumlahan
+# # # # # # # # # puts hasil_plus
+# # # # # # # # # hasil_minus = calc.perkalian
+# # # # # # # # # puts hasil_minus
+
+
+# # # # # # # # # class Print
+# # # # # # # # #     def initialize(text)
+# # # # # # # # #         @text =text
+# # # # # # # # #     end
+# # # # # # # # #     def print
+# # # # # # # # #         puts @text
+# # # # # # # # #     end
+# # # # # # # # # end
+
+# # # # # # # # # printer = Print.new("Saya sedang belajar getter-setter")
+# # # # # # # # # printer.print
+
+# # # # # # # # # printer = Print.new("Saya sedang mendalami getter -setter")
+# # # # # # # # # printer.print
+# # # # # # # # # puts"=================================="
+
+# # # # # # # # # class Print
+# # # # # # # # #     def initialize(text)
+# # # # # # # # #         @text = text
+# # # # # # # # #     end
+# # # # # # # # #     def text = (kalimat)
+# # # # # # # # #         @text = kalimat
+# # # # # # # # #     end
+# # # # # # # # #     def text
+# # # # # # # # #         @text
+# # # # # # # # #     end
+# # # # # # # # # end
+
+# # # # # # # # class Print 
+# # # # # # # #     attr_accessor :text
+# # # # # # # #     attr_writer :text #setter
+# # # # # # # #     attr_reader :text #getter
+
+# # # # # # # #     def initialize(text)
+# # # # # # # #         # @text = text
+# # # # # # # #         @teks = text
+# # # # # # # #     end
+# # # # # # # #     def text=(text)
+# # # # # # # #         @text = text
+# # # # # # # #     end
+# # # # # # # #     #getter
+# # # # # # # #     def text
+# # # # # # # #         @text
+# # # # # # # #     end
+# # # # # # # #     def print
+# # # # # # # #         # puts "mencetak : #{@text}"
+# # # # # # # #         puts "mencetak : #{@teks}"
+# # # # # # # #     end
+# # # # # # # # end
+
+# # # # # # # # printer = Print.new("Saya sedang belajar ruby")
+# # # # # # # # printer.print
+# # # # # # # # printer.text = "TExt kedua"
+# # # # # # # # printer.print
+# # # # # # # # printer.text = "TExt kedua edit"
+# # # # # # # # puts printer.print
+
+# # # # # # # # puts "=================================="
+
+# # # # # # # # class Print 
+# # # # # # # #     # attr_accessor :text
+# # # # # # # #     # attr_writer :text #setter
+# # # # # # # #     attr_reader :text #getter
+
+# # # # # # # #     def initialize(text)
+# # # # # # # #         # @text = text
+# # # # # # # #         @text = text
+# # # # # # # #     end
+# # # # # # # #     def text=(text)
+# # # # # # # #         @text = text.upcase
+# # # # # # # #     end
+# # # # # # # #     #getter
+# # # # # # # #     # def text
+# # # # # # # #     #     @text
+# # # # # # # #     # end
+# # # # # # # #     def print
+# # # # # # # #         # puts "mencetak : #{@text}"
+# # # # # # # #         puts "mencetak : #{@text}"
+# # # # # # # #     end
+# # # # # # # # end
+
+# # # # # # # # printer = Print.new("Saya sedang belajar ruby")
+# # # # # # # # printer.print
+# # # # # # # # printer.text = "TExt kedua"
+# # # # # # # # printer.print
+# # # # # # # # printer.text = "TExt kedua edit"
+# # # # # # # # puts printer.print
+
+
+# # # # # # # # Atribut Reader
+# # # # # # # # class MesinPencetak
+# # # # # # # #     attr_reader :text, :duration
+# # # # # # # #     def initialize(text, duration)
+# # # # # # # #         @text = text
+# # # # # # # #         @duration = duration
+# # # # # # # #     end
+# # # # # # # #     def cetak
+# # # # # # # #         puts "Hari ini saya belajar method #{text}"
+# # # # # # # #         puts "Waktu yang diperlukan untuk belajarnya adalah #{@duration}"
+# # # # # # # #     end
+# # # # # # # # end
+
+# # # # # # # # mesinku = MesinPencetak.new("getter", "sebentar")
+# # # # # # # # mesinku.cetak
+# # # # # # # # puts mesinku.text
+# # # # # # # # puts mesinku.duration
+
+
+# # # # # # # class MesinPencetak
+# # # # # # #     attr_reader :text
+# # # # # # #     attr_writer :text
+# # # # # # #     def initialize(text)
+# # # # # # #         @text = text
+# # # # # # #     end
+# # # # # # #     def cetak 
+# # # # # # #         "Hari ini saya beljar method #{@text}"
+# # # # # # #     end
+# # # # # # # end
+# # # # # # # mesinku = MesinPencetak.new("getter")
+# # # # # # # puts mesinku.cetak
+# # # # # # # puts mesinku.text #KLo tanpa getter gk bisa
+# # # # # # # mesinku.text = "setter"
+# # # # # # # puts mesinku.text#KLo tanpa setter gk bisa
+# # # # # # # puts mesinku.cetak
+
+# # # # # # class MesinPencetak
+# # # # # #     attr_reader :text, :duration
+# # # # # #     #Fungsinya untuk mengambil attribut/instant varibale yang diizinkan
+# # # # # #     attr_writer :text, :duration
+# # # # # #     #Fungsinya untuk mengubah attribut/instant varibale yang diizinkan
+
+# # # # # #     def initialize(text, duration)
+# # # # # #         @text = text
+# # # # # #         @duration = duration
+# # # # # #     end
+# # # # # #     def cetak 
+# # # # # #         puts "Hari ini saya belajar method #{@text}"
+# # # # # #         puts "Waktu yang diperlukan untuk belajarnya adalah #{@duration}"
+# # # # # #     end
+# # # # # # end
+# # # # # # mesinku = MesinPencetak.new("getter", "lama")
+# # # # # # mesinku.cetak
+
+# # # # # # mesinku.text = "setter"
+# # # # # # mesinku.duration = "semenit"
+# # # # # # mesinku.cetak
+
+# # # # # class MesinPencetak
+# # # # #     attr_accessor :text, :duration
+# # # # #     def initialize(text, duration)
+# # # # #                 @text = text
+# # # # #                 @duration = duration
+# # # # #             end
+# # # # #             def cetak 
+# # # # #                 puts "Hari ini saya belajar method #{@text}"
+# # # # #                 puts "Waktu yang diperlukan untuk belajarnya adalah #{@duration}"
+# # # # #             end
+# # # # # end
+# # # # # mesinku = MesinPencetak.new("getter", "lama")
+# # # # # mesinku.cetak
+
+# # # # # mesinku.text = "setter"
+# # # # # mesinku.duration = "semenit"
+# # # # # mesinku.cetak
+
+# # # # class MesinPencetak
+# # # #     attr_accessor :text
+# # # #     def initialize(text)
+# # # #         @text = text
+# # # #     end
+# # # #     def cetak(format)
+# # # #         cekformat(@text, format)
+# # # #     end
+# # # #     private 
+# # # #     def cekformat(text, format)
+# # # #         if format == :plain
+# # # #             text
+             
+# # # #         elsif format == :blink
+# # # #             "*** #{text} ***"
+# # # #         end
+# # # #     end
+# # # #     mesinku = MesinPencetak.new("Belajar ruby dan ruby on rails")
+# # # #     puts mesinku.cetak(:blink)
+# # # #     puts mesinku.cetak(:plain)
+    
+# # # #     # puts mesinku.cekformat("tes", :blink)
+# # # # end
+
+# # # #Inheritance
+
+# # # class Siswa
+# # #     attr_accessor :nama, :materi, :nilai
+# # #     def initialize(nama, materi, nilai)
+# # #         @nama = nama
+# # #         @materi = materi
+# # #         @nilai = nilai
+# # #     end
+# # #     def belajar
+# # #         puts "Siswa #{@nama} sedang belajar #{@materi}"
+# # #     end
+# # #     def ulangan 
+# # #         puts "Siswa #{@nama} mendapatkan nilai #{@nilai} untuk materi #{materi}"
+# # #     end
+# # # end
+
+# # # class Kelas12 < Siswa
+# # #     attr_accessor :mapel, :nilai_un
+# # #     def initialize(mapel, nilai_un)
+# # #         @mapel = mapel
+# # #         @nilai_un = nilai_un
+# # #     end
+# # #     def unbk
+# # #         puts "Siswa #{@nama} mengerjakan UN mapel #{@mapel} dengan nilai #{nilai_un}"
+# # #     end
+# # # end
+
+# # # sutera = Siswa.new("Sutera", 'Ruby', 10)
+# # # sutera.belajar
+# # # sutera.ulangan
+
+# # # mutia = Kelas12.new('Matematika', 10)
+# # # mutia.nama = "Mutia"
+# # # mutia.materi = "RUby On Rails"
+# # # mutia.nilai = 0
+# # # mutia.belajar
+# # # mutia.ulangan
+# # # mutia.unbk
+
+# # class Siswa
+# #     attr_accessor :nama, :materi, :nilai
+# #     def initialize(nama, materi, nilai)
+# #         @nama = nama
+# #         @materi = materi
+# #         @nilai = nilai
+# #     end
+# #     def belajar
+# #         puts "Siswa #{@nama} sedang belajar #{@materi}"
+# #     end
+# #     def ulangan 
+# #         puts "Siswa #{@nama} mendapatkan nilai #{@nilai} untuk materi #{materi}"
+# #     end
+# # end
+
+# # class Kelas12 < Siswa
+# #     attr_accessor :mapel, :nilai_un
+# #     def initialize(mapel, nilai_un)
+# #         @mapel = mapel
+# #         @nilai_un = nilai_un
+# #     end
+# #     def belajar
+# #         puts "Siswa #{@nama} belajar #{materi} dengan serius."
+# #     end
+# #     def unbk
+# #         puts "Siswa #{@nama} mengerjakan UN mapel #{@mapel} dengan nilai #{nilai_un}"
+# #     end
+# # end
+
+# # mutia = Kelas12.new("Matematika", 10)
+# # mutia.nama = "Mutia"
+# # mutia.materi = "RUby on rails"
+# # mutia.nilai = 9
+# # mutia.belajar
+
+
+# class ApiConnection
+#     attr_accessor :url, :name
+#     def initialize(url, name)
+#         @url = url
+#         @name = name
 #     end
-#     def pengurangan(a)
-#         a - 10
+#     def connect
+#         @connect = true #Logika program disini
+#     end
+#     def status
+#         if @connect
+#             @connected
+#         else
+#             @unconnected
+#         end
 #     end
 # end
-# calc = Calculator.new
-# hasil_penjumlahan = calc.penjumlahan(18, 5)
-# puts hasil_penjumlahan
-# hasil_pengurangan = calc.pengurangan(18)
-# puts hasil_pengurangan
 
-# class CalculatorKool
-#     def initialize(a, b)
-#         @parameter_a = a
-#         @parameter_b = b
-#         @lokal = a + b
+# class FacebookConnection < ApiConnection
+#    def version 
+#     1
+#    end
+#    def status
+#        :connected
+#    end
+# end
+# facebook_connection = FacebookConnection.new("http://facebook.com", 'facebook')
+
+# status = facebook_connection.status
+# version = facebook_connection.version
+
+# puts status
+# puts version
+
+
+#Super Method
+
+# class ApiConnection
+#     attr_accessor :url, :name
+#     def initialize(url, name)
+#         @url = url
+#         @name = name
 #     end
-#     def penjumlahan
-#         @parameter_a + @parameter_b
+#     def connect
+#         @connect = true #Logika program disini
 #     end
-#     def perkalian
-#         @parameter_a * @parameter_b
+#     def status
+#         if @connect
+#             @connected
+#         else
+#             @unconnected
+#         end
 #     end
 # end
 
-# calc = CalculatorKool.new(10, 5) #Giving parameter Default variable
-# hasil_plus = calc.penjumlahan
-# puts hasil_plus
-# hasil_minus = calc.perkalian
-# puts hasil_minus
-
-
-# class Print
-#     def initialize(text)
-#         @text =text
+# class FacebookConnection < ApiConnection
+#     def version 
+#      1
 #     end
-#     def print
-#         puts @text
+#     def connect
+#         super
+#         puts 'connection establisjed'
 #     end
-# end
-
-# printer = Print.new("Saya sedang belajar getter-setter")
-# printer.print
-
-# printer = Print.new("Saya sedang mendalami getter -setter")
-# printer.print
-# puts"=================================="
-
-# class Print
-#     def initialize(text)
-#         @text = text
+#     def status
+#         super
 #     end
-#     def text = (kalimat)
-#         @text = kalimat
-#     end
-#     def text
-#         @text
-#     end
-# end
+#  end
 
-class Print 
-    attr_accessor :text
-    attr_writer :text #setter
-    attr_reader :text #getter
+# facebook_connection = FacebookConnection.new("http://facebook.com", 'facebook')
 
-    def initialize(text)
-        # @text = text
-        @teks = text
+# facebook_connection.connect
+# status = facebook_connection.status
+# version = facebook_connection.version
+
+# puts status
+# puts version
+
+
+#Class Self
+class ApiConnection
+    attr_accessor :url, :name
+    def initialize(url, name)
+        @url = url
+        @name = name
     end
-    def text=(text)
-        @text = text
+    def connect
+        @connect = true #Logika program disini
     end
-    #getter
-    def text
-        @text
-    end
-    def print
-        # puts "mencetak : #{@text}"
-        puts "mencetak : #{@teks}"
+    def status
+        if @connect
+            @connected
+        else
+            @unconnected
+        end
     end
 end
 
-printer = Print.new("Saya sedang belajar ruby")
-printer.print
-printer.text = "TExt kedua"
-printer.print
-printer.text = "TExt kedua edit"
-puts printer.print
-
-puts "=================================="
-
-class Print 
-    # attr_accessor :text
-    # attr_writer :text #setter
-    attr_reader :text #getter
-
-    def initialize(text)
-        # @text = text
-        @text = text
-    end
-    def text=(text)
-        @text = text.upcase
-    end
-    #getter
-    # def text
-    #     @text
-    # end
-    def print
-        # puts "mencetak : #{@text}"
-        puts "mencetak : #{@text}"
-    end
+class FacebookConnection < ApiConnection
+    class << self
+        def version 
+         2
+        end
+        def status
+            :connected
+        end
+    end    
 end
 
-printer = Print.new("Saya sedang belajar ruby")
-printer.print
-printer.text = "TExt kedua"
-printer.print
-printer.text = "TExt kedua edit"
-puts printer.print
+puts FacebookConnection.version #Klo pake self untuk akses variablenya gk perlu pake new kecuali parentnya ex : ApiConnection
+puts FacebookConnection.status
+
+facebook_connection = FacebookConnection.new("http://facebook.com", 'facebook')
+puts FacebookConnection.version
+puts FacebookConnection.status
+
+
+
--- test2.rb
+++ test2.rb
@@ -1,59 +1,59 @@
-# # # # hari = "senin"
-# # # # hari.upcase!
+# # # # # hari = "senin"
+# # # # # hari.upcase!
 
-# # # # puts hari
+# # # # # puts hari
 
-# # # # hari.reverse!
-# # # # puts hari
+# # # # # hari.reverse!
+# # # # # puts hari
 
-# # # # inputan = gets.chomp
+# # # # # inputan = gets.chomp
 
-# # # # puts "Hari ini adalah hari #{inputan}"
-# # # # puts "Hari ini adalah hari #{inputan}".gsub('hari', inputan)
+# # # # # puts "Hari ini adalah hari #{inputan}"
+# # # # # puts "Hari ini adalah hari #{inputan}".gsub('hari', inputan)
 
 
-# # # def fungsi(item)
-# # #     4.downto(0) do |i|
-# # #         puts item[i]
-# # #     end
+# # # # def fungsi(item)
+# # # #     4.downto(0) do |i|
+# # # #         puts item[i]
+# # # #     end
+# # # # end
+
+# # # # arr = ["siapa","rumah","lokasi","disana", "Daerah"]
+# # # # fungsi(arr)
+
+# # # def set(a, b, c, d)
+# # #     puts d
+# # #     puts b
+# # #     puts c
+# # #     puts a
 # # # end
+# # # arr = ["Hutan", "gunung", "pantai", "jalanan"]
+# # # # puts *arr
+# # # set(*arr)
 
-# # # arr = ["siapa","rumah","lokasi","disana", "Daerah"]
-# # # fungsi(arr)
-
-# # def set(a, b, c, d)
-# #     puts d
-# #     puts b
-# #     puts c
-# #     puts a
+# # arr = []
+# # 1.upto(20) do |i|
+# #     arr.push(i)
 # # end
-# # arr = ["Hutan", "gunung", "pantai", "jalanan"]
-# # # puts *arr
-# # set(*arr)
+# # puts arr[14]
+# # arr[3] = "abcd"
+# # arr[0] = 1 + 0.5
+# # arr.delete_at(10)
+# # 11.upto(14) do |x|
+# #     arr[x] = "d"
+# # end
+
+# # puts "#{arr.to_s.gsub('1', '')}"
 
 # arr = []
-# 1.upto(20) do |i|
+# 4.upto(9) do |i|
 #     arr.push(i)
 # end
-# puts arr[14]
-# arr[3] = "abcd"
-# arr[0] = 1 + 0.5
-# arr.delete_at(10)
-# 11.upto(14) do |x|
-#     arr[x] = "d"
+# arr.delete_if do |a|
+#     a < 7
 # end
 
-# puts "#{arr.to_s.gsub('1', '')}"
-
-arr = []
-4.upto(9) do |i|
-    arr.push(i)
-end
-arr.delete_if do |a|
-    a < 7
-end
-
-puts arr.join('x')
+# puts arr.join('x')
 
 
 
